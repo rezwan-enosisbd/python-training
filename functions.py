bb254192adf3cc0a1286b33a86acf4821a39e1b2
Rezwan Al Kaoser <rezwan.kaoser@enosisbd.com>
2022-08-17 20:28:33 +0600

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+
+
+def sayMyName(first_name, last_name, expertise = None):
+  print(f'My Name is {first_name} {last_name}')
+
+
+sayMyName('Rezwan', 'Kaoser')
+sayMyName('Rezwan', last_name='kaoser')
+
+# ***** put keyword argument after positional argument
