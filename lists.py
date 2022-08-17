369450369084873eff2549af57aac34653f99ced
Rezwan Al Kaoser <rezwan.kaoser@enosisbd.com>
2022-08-17 19:18:24 +0600

    list

---
+++ lists.py
@@ -0,0 +1,19 @@
+l1 = [ 1, 2, 3, 3, 3 ]
+
+l1.append(33)
+print(l1)
+
+l1.insert(1, 4444)
+print(l1)
+
+l1.remove(3)
+print(l1)
+
+l1.pop(3)
+print(l1)
+
+l1.pop(3)
+print(l1)
+
+print(l1.index(22))
+
