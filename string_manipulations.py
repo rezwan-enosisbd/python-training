9cf50316c67c4ce6b986f6c0363d3a2ed7cb369d
Rezwan Al Kaoser <rezwan.kaoser@enosisbd.com>
2022-08-17 18:03:21 +0600

    string manipulations

---
+++ string_manipulations.py
@@ -0,0 +1,16 @@
+intro1 = "Python's beginners guide" # double quote
+intro2 = 'Name of him is "Rezwan"' # single quote
+
+para = '''
+Name: Rezwan Al Kaoser
+Expertise: Python
+''' # multi line string
+
+print(intro2[0]) # it should show first character of intro 'N'// indexing
+
+name = "Rezwan"
+
+print(name[1:-1]) # negetive index start from end , last char is -1 index. It should print 'ezwa'
+
+print(name[0:2]) # 0 to 1 index , that is 'Re'
+
