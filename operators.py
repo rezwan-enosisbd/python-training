9d89be3544c4f6681f8f3a5fd1adc1ca69809033
Rezwan Al Kaoser <rezwan.kaoser@enosisbd.com>
2022-08-17 18:26:51 +0600

    operators

---
+++ operators.py
@@ -0,0 +1,10 @@
+num1 = 4
+num2 = 2.5
+
+print(f'{num1} + {num2} = {num1+num2}')
+print(f'{num1} - {num2} = {num1-num2}')
+print(f'{num1} * {num2} = {num1*num2}')
+print(f'{num1} / {num2} = {num1/num2}') # return float
+print(f'{num1} // {num2} = {num1//num2}') # return int
+print(f'{num1} ** {num2} = {num1**num2}') # exponential 4^2.5
+
