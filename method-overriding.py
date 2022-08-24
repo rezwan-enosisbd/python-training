0adbe192a93779355ba1fa2a1429d9de3e09846d
Rezwan Al Kaoser <rezwan.kaoser@enosisbd.com>
2022-08-24 10:59:00 +0600

    method overriding

---
+++ method-overriding.py
@@ -0,0 +1,37 @@
+class Calculator:
+    """Do addition, subtraction, multiplication and division."""
+
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return 'It is impossible to divide by zero.'
+
+class SuperCalculator(Calculator):
+    """Do addition, subtraction, multiplication, division, square and cube."""
+
+    def addition(self, a, b, c):
+        return a + b + c
+
+    def square(self, a):
+        return a * a
+
+    def cube(self, a):
+        return a * a * a
+
+my_calculator = SuperCalculator()
+
+temp = my_calculator.addition(23, 47, 12)
+print(temp)
+
+temp = my_calculator.addition(23, 10)
+print(temp)
